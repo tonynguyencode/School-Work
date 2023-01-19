2f044e2a33d6efd60aab12cbefe28675112befb6
Tony Nguyen <tnguyen64@email.davenport.edu>
2023-01-18 18:37:42 -0800

    Add Week2 Project

---
+++ character_Arrayweek2/Character_Frequency/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Character_Frequency
+{
+    public class CharacterFrequency
+    {
+        private char _character;
+        private int _frequency;
+
+        public CharacterFrequency()
+        {
+            this._character = '\x0000';
+            this._frequency = 1;
+        }
+
+
+        public CharacterFrequency(char character)
+        {
+            this._character = character;
+            this._frequency = 1;
+        }
+
+        public CharacterFrequency(CharacterFrequency character)
+        {
+            this._character = character._character;
+            this._frequency = character._frequency;
+        }
+
+        public char Character
+        {
+            get { return this._character; }
+            set { this._character = value; }
+        }
+
+        public int Frequency
+        {
+            get { return this._frequency; }
+            set { this._frequency = value; }
+        }
+        public void Increment()
+        {
+            this._frequency += 1;
+        }
+        public override int GetHashCode()
+        {
+            return (int)_frequency;
+        }
+        public override string ToString()
+        {
+            return $"{this._character} ({(int)_character})      Frequency: {this._frequency}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) { return false; }
+            if (obj == this) { return true; }
+
+            //Cast obj to a student. If the cast cannot be performed, student variable will be set to null
+            CharacterFrequency variable = obj as CharacterFrequency;
+
+            if (variable == null)
+            {
+                return false;
+            }
+            return _character.Equals(variable._character);
+        }
+        static void Main()
+        {
+            
+        }
+    }
+}
---
+++ character_Arrayweek2/character_Arrayweek2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Character_Frequency;
+using System.IO;
+using System.Xml.Linq;
+
+namespace character_Arrayweek2
+{
+    internal class Program
+    {
+        public static CharacterFrequency character_Frequency;
+        public static ArrayList arrayVector = new ArrayList();
+        private static bool Check_valid(char value)
+        {
+            int value_byte = (int)value;
+            if (value >= 33)
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter your input full path: ");  //Getting the appropriate path
+            string read_file = Console.ReadLine();
+            string fileName = $@"{read_file}";
+
+            //Using BinaryWriter to write the output txt
+            //Getting the path or create a path
+            Console.WriteLine("Enter your output file path: ");
+            string output_file = Console.ReadLine();
+            string file_output_Name = $@"{output_file}";
+
+            //Got the path.
+            //Using BinaryReader to read the input txt
+
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    FileInfo file_info = new FileInfo(fileName);
+                    //Using the array only to store the ASCII object
+                    long size = 0;
+                    arrayVector = new ArrayList((int)file_info.Length);
+                    int array_position = 0;
+                    //This is where calculation will be make.
+                    using (var stream = File.Open(fileName, FileMode.Open)) //Open the file and stream
+                    {
+                        using (var reader = new BinaryReader(stream, Encoding.UTF8, false))      //Using BinaryReader method
+                        {
+                            while (size < file_info.Length)
+                            {
+                                char c = (char)reader.Read();
+                                
+                                size++;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid File!");
+                }
+            } catch (Exception eex) { Console.WriteLine(eex.Message); }
+
+        }
+    }
+}
